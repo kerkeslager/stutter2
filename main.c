e79587bc2fb2fa88b9d2d7d86fa85b927f678742
David Kerkeslager <kerkeslager@gmail.com>
2016-05-28 17:09:03 +0000

    Added symbols

--- main.c
+++ main.c
@@ -43,12 +43,14 @@ char* itoa(int n, char s[])
 
 /* begin types */
 struct String;
+struct Symbol;
 
 enum Tag;
 union Instance;
 struct Object;
 
 typedef struct String String;
+typedef struct Symbol Symbol;
 
 typedef enum Tag Tag;
 typedef union Instance Instance;
@@ -59,16 +61,23 @@ struct String
   char* characters;
 };
 
+struct Symbol
+{
+  char* name;
+};
+
 enum Tag
 {
   INTEGER,
-  STRING
+  STRING,
+  SYMBOL
 };
 
 union Instance
 {
   int32_t integer;
   String string;
+  Symbol symbol;
 };
 
 struct Object
@@ -113,6 +122,13 @@ void dereferenceObject(Object* self)
       free(self->instance.string.characters);
       free(self);
       break;
+
+    case SYMBOL:
+      break;
+
+    default:
+      printf("Dereference object called with unexpected type");
+      exit(EXIT_FAILURE);
   }
 }
 /* end dereferencers */
@@ -185,14 +201,49 @@ ParseResult stringParser(char* source)
   return result;
 }
 
+bool isSymbolCharacter(char character)
+{
+  return 'a' <= character && character <= 'z';
+}
+
+ParseResult symbolParser(char* source)
+{
+  ParseResult result;
+  result.succeeded = false;
+  result.result = NULL;
+  result.remaining = source;
+
+  if(!isSymbolCharacter(*(result.remaining)))
+  {
+    return result;
+  }
+
+  while(isSymbolCharacter(*(result.remaining)))
+  {
+    result.remaining++;
+  }
+
+  size_t memoryLength = result.remaining - source + 1;
+
+  result.succeeded = true;
+  result.result = malloc(sizeof(Object));
+  result.result->referenceCount = 1;
+  result.result->tag = SYMBOL;
+  result.result->instance.symbol.name = memcpy(malloc(memoryLength), source, memoryLength - 1);
+  result.result->instance.symbol.name[memoryLength - 1] = '\0';
+
+  return result;
+}
+
 ParseResult objectParser(char* source)
 {
-#define PARSER_COUNT 2
+#define PARSER_COUNT 3
 
   ParseResult (*parsers[PARSER_COUNT])(char*) =
   {
     integerParser,
-    stringParser
+    stringParser,
+    symbolParser
   };
 
   ParseResult result;
@@ -212,7 +263,9 @@ Object* parse(ParseResult (*parser)(char*), char* source)
 {
   ParseResult result = parser(source);
   if(result.succeeded) return result.result;
-  return NULL;
+
+  printf("Unable to parse source \"%s\".", source);
+  exit(EXIT_FAILURE);
 }
 /* end parser */
 
@@ -221,7 +274,19 @@ Object* evaluate1(Object* object)
 {
   if(object == NULL) return NULL;
 
-  return rereferenceObject(object);
+  switch(object->tag)
+  {
+    case INTEGER:
+    case STRING:
+      return rereferenceObject(object);
+
+    case SYMBOL:
+      return NULL;
+
+    default:
+      printf("Unexpected object type passed to evaluate1");
+      exit(EXIT_FAILURE);
+  }
 }
 
 Object* show1(Object* object)
@@ -250,6 +315,13 @@ Object* show1(Object* object)
 
       string.characters = malloc(memoryLength);
       snprintf(string.characters, memoryLength, "\"%s\"", object->instance.string.characters);
+      break;
+
+    case SYMBOL:
+      memoryLength = strlen(object->instance.symbol.name) + 2;
+
+      string.characters = malloc(memoryLength);
+      snprintf(string.characters, memoryLength, ":%s", object->instance.symbol.name);
       break;
   }
 
